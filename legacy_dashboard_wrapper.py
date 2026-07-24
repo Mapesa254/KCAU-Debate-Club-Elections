3d44ada110d5ff0c6a960b051137e6d4f6375911
Benjamin Mapesa <benleomapesa@gmail.com>
2026-07-24 12:26:32 +0300

    style: update mobile header responsiveness and adjust topbar layout

--- legacy_dashboard_wrapper.py
+++ legacy_dashboard_wrapper.py
@@ -24,8 +24,16 @@ def main():
         layout="wide",
     )
 
-
-
+    st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 0rem;
+                    padding-right: 0rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
     setup_static_dashboard()
     
     # Streamlit serves the static folder at /app/static/
